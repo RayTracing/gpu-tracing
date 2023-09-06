c20800aa1baeb1eb7a869f88465f3652746165a4
Arman Uguray <arman.uguray@gmail.com>
2023-09-06 14:49:57 -0700

    Finish the first chapter and code template

--- code/template/src/main.rs
+++ code/template/src/main.rs
@@ -12,7 +12,7 @@ use {
     winit::{
         event::{Event, WindowEvent},
         event_loop::{ControlFlow, EventLoop},
-        window::WindowBuilder,
+        window::{Window, WindowBuilder},
     },
 };
 
@@ -25,8 +25,13 @@ async fn main() -> Result<()> {
     let window_size = winit::dpi::LogicalSize::new(WIDTH, HEIGHT);
     let window = WindowBuilder::new()
         .with_inner_size(window_size)
+        .with_resizable(false)
         .with_title("GPU Path Tracer".to_string())
         .build(&event_loop)?;
+
+    let (device, queue, surface) = connect_to_gpu(&window).await?;
+
+    // TODO: initialize renderer
 
     event_loop.run(move |event, _, control_flow| {
         *control_flow = ControlFlow::Poll;
@@ -36,7 +41,14 @@ async fn main() -> Result<()> {
                 _ => (),
             },
             Event::RedrawRequested(_) => {
+                // Wait for the next available frame buffer.
+                let frame: wgpu::SurfaceTexture = surface
+                    .get_current_texture()
+                    .expect("failed to get current texture");
+
                 // TODO: draw frame
+
+                frame.present();
             }
             Event::MainEventsCleared => {
                 // draw repeatedly
@@ -45,4 +57,55 @@ async fn main() -> Result<()> {
             _ => (),
         }
     });
+}
+
+async fn connect_to_gpu(window: &Window) -> Result<(wgpu::Device, wgpu::Queue, wgpu::Surface)> {
+    use wgpu::TextureFormat::{Bgra8Unorm, Rgba8Unorm};
+
+    // Create an "instance" of wgpu. This is the entry-point to the API.
+    let instance = wgpu::Instance::default();
+
+    // Create a drawable "surface" that is associated with the window.
+    let surface = unsafe { instance.create_surface(&window) }?;
+
+    // Request a GPU that is compatible with the surface. If the system has multiple GPUs then
+    // pick the high performance one.
+    let adapter = instance
+        .request_adapter(&wgpu::RequestAdapterOptions {
+            power_preference: wgpu::PowerPreference::HighPerformance,
+            force_fallback_adapter: false,
+            compatible_surface: Some(&surface),
+        })
+        .await
+        .context("failed to find a compatible adapter")?;
+
+    // Connect to the GPU. "device" represents the connection to the GPU and allows us to create
+    // resources like buffers, textures, and pipelines. "queue" represents the command queue that
+    // we use to submit commands to the GPU.
+    let (device, queue) = adapter
+        .request_device(&wgpu::DeviceDescriptor::default(), None)
+        .await
+        .context("failed to connect to the GPU")?;
+
+    // Configure the texture memory backs the surface. Our renderer will draw to a surface texture
+    // every frame.
+    let caps = surface.get_capabilities(&adapter);
+    let format = caps
+        .formats
+        .into_iter()
+        .find(|it| matches!(it, Rgba8Unorm | Bgra8Unorm))
+        .context("could not find preferred texture format (Rgba8Unorm or Bgra8Unorm)")?;
+    let size = window.inner_size();
+    let config = wgpu::SurfaceConfiguration {
+        usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+        format,
+        width: size.width,
+        height: size.height,
+        present_mode: wgpu::PresentMode::AutoVsync,
+        alpha_mode: caps.alpha_modes[0],
+        view_formats: vec![],
+    };
+    surface.configure(&device, &config);
+
+    Ok((device, queue, surface))
 }
